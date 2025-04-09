0b052b636f4bc59f2acf0d66b682bdfbeb384713
faarhn <91136756+faarhn@users.noreply.github.com>
2025-04-10 01:11:10 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -63,7 +63,7 @@ document.addEventListener('DOMContentLoaded',()=>{
 
          console.log(li.id)
 
-         li.className = "px-2 mt-2 flex font-bold justify-between border-2 border-black rounded-xl gap-2"
+            li.className = "px-4 py-2 mt-2 flex flex-wrap justify-between items-center font-semibold border-2 border-black rounded-xl gap-2 bg-white shadow"; 
 
         document.querySelector("li");
         let del_btn = document.createElement("button");
@@ -72,7 +72,8 @@ document.addEventListener('DOMContentLoaded',()=>{
          del_btn.id = task.id +"_btn"
          console.log(del_btn.id)
          del_btn.textContent = "Delete";
-         del_btn.className = " px-2 mr-2 border-2 bg-2 bg-red-500 border-black rounded-xl"
+          del_btn.className = "px-3 py-1 bg-red-500 text-white border-2 border-black rounded-xl hover:bg-red-600 transition"; 
+
 
          del_btn.addEventListener("click",function(){
 
